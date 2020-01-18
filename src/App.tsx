7af0087da6b592e998d3f3beb2efe6686023bdfd
vfportero <vfportero@gmail.com>
2020-01-18 17:55:11 +0100

    Ignored firebaseconfig file

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,12 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 import * as firebase from 'firebase';
+import * as firebaseConfig from './firebaseconfig';
 
 
 const App: React.FC = () => {
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyDID8wQBuZCVAXZXWN3PoMidd_mKJkAH8M",
-    authDomain: "starfinder-character-cre-2b9fc.firebaseapp.com",
-    databaseURL: "https://starfinder-character-cre-2b9fc.firebaseio.com",
-    projectId: "starfinder-character-cre-2b9fc",
-    storageBucket: "starfinder-character-cre-2b9fc.appspot.com",
-    messagingSenderId: "257636364437",
-    appId: "1:257636364437:web:9fd01be19801a036dfeb9c"
-  };
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
