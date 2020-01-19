723e3f54d564b5a62fd0a8e6de249c3c47aee95d
vfportero <vfportero@gmail.com>
2020-01-19 20:26:14 +0100

    Include firebaseconfig (is not a security hole and is needed to run the test on CI)

---
+++ src/firebaseconfig.ts
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDID8wQBuZCVAXZXWN3PoMidd_mKJkAH8M",
+    authDomain: "starfinder-character-cre-2b9fc.firebaseapp.com",
+    databaseURL: "https://starfinder-character-cre-2b9fc.firebaseio.com",
+    projectId: "starfinder-character-cre-2b9fc",
+    storageBucket: "starfinder-character-cre-2b9fc.appspot.com",
+    messagingSenderId: "257636364437",
+    appId: "1:257636364437:web:9fd01be19801a036dfeb9c",
+  };
